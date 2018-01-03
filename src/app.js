15b9f957a29a474cd2b3c8e7ca80846a07526429
Maire Chew <mairechew@gmail.com>
2018-01-03 12:32:38 -0700

    working on inheritance examples

--- src/app.js
+++ src/app.js
@@ -1,35 +1,15 @@
-// used to be constructor functions in ES5
-class Drone {
-// avoid heavy processing
-  constructor(id, name){
-  // using 'this' references the specific instanceof object
-  // instance properties
-  // underscore convention means private property
-    this._id = id;
-    this.name = name;
-  }
-  // static methods only exists on the class Drone
-  static getCompany() {
-    console.log(this.id);
-  }
+class Vehicle {
 
-  fly() {
-    console.log('Drone ' + this.id + ' is flying');
-  }
-  // getter function
-  // used for validation or loggings
-  get id() {
-    console.log('in id getter');
-    return this._id + ' TEMPORARY';
-  }
+}
+class Drone extends Vehicle {
 
-  // setter functions
-  // used to set a value or change data
-  set id(value) {
-    this._id = value;
+}
+
+class Car extends Vehicle {
+  constructor() {
+    super();
+    console.log('constructing Car');
   }
 }
 
-let drone = new Drone('A123');
-drone.id = 'B456'
-console.log('drone id: ' + drone.id);
+let c = new Car();
